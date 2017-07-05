66186d137e65dbe5d97d11b7a49bf8d3ef8b792f
bill <tuntunkimpo@naver.com>
2017-07-06 00:38:12 +0900

    session 활용한 로그인생활코딩 예제

--- pieces/vote/process.php
+++ pieces/vote/process.php
@@ -3,6 +3,7 @@ require("config/config.php");
 require("lib/db.php");
 $conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
 $result = mysqli_query($conn, "SELECT votenum FROM num WHERE id=1");
+
 $row = mysqli_fetch_assoc($result);
 //echo htmlspecialchars($row['votenum']);
 $plusone = ++$row['votenum'];
